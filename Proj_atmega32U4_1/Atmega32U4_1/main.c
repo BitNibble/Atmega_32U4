8fb10e38fb66822d5ff43606a0b0f662a51287fa
BitNibble <sergio.salazar.santos@gmail.com>
2023-04-26 17:01:09 +0100

    Atmega 32U4

---
+++ Proj_atmega32U4_1/Atmega32U4_1/main.c
@@ -0,0 +1,31 @@
+/*
+ * main.c
+ *
+ * Created: 11/29/2022 7:54:12 PM
+ *  Author: sergi
+ *	License: GNU General Public License
+ */ 
+#define F_CPU 16000000UL
+/*
+** library
+*/
+#include <xc.h>
+#include <inttypes.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <inttypes.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include "explode.h"
+#include "keypad.h"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
