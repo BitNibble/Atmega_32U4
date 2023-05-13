6947235f16d4c09c4c63b46438394a9bb918272a
BitNibble <sergio.salazar.santos@gmail.com>
2023-05-14 00:05:47 +0100

    32U4_lib-1

---
+++ Proj_atmega32U4_1/Atmega32U4_1/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Library\atmega32u4mapping.c
+
+Library\atmegaeeprom.c
+
+Library\explode.c
+
+main.c
+
