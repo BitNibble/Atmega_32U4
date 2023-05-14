042714a4603a7e67b5cf8a77ad08e0a60cb56d2e
BitNibble <sergio.salazar.santos@gmail.com>
2023-05-14 15:10:41 +0100

    32U4_lib-6

---
+++ Proj_atmega32U4_1/Atmega32U4_1/Debug/Library/atmegaeeprom.d
@@ -0,0 +1,49 @@
+Library/atmegaeeprom.d Library/atmegaeeprom.o: ../Library/atmegaeeprom.c \
+ ../Library/atmegaeeprom.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\eeprom.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\io.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\sfr_defs.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\inttypes.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\features.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\stdint.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\bits\alltypes.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\bits\stdint.h \
+ C:/Program\ Files\ (x86)/Atmel/Studio/7.0/Packs/Atmel/ATmega_DFP/2.0.401/xc8/avr/include/avr/iom32u4.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\portpins.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\common.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\fuse.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\lock.h \
+ c:\program\ files\microchip\xc8\v2.41\avr\lib\gcc\avr\5.4.0\include\stddef.h
+
+../Library/atmegaeeprom.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\eeprom.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\io.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\inttypes.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\features.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\bits\alltypes.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\bits\stdint.h:
+
+C:/Program\ Files\ (x86)/Atmel/Studio/7.0/Packs/Atmel/ATmega_DFP/2.0.401/xc8/avr/include/avr/iom32u4.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\portpins.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\common.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\fuse.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\avr\include\avr\lock.h:
+
+c:\program\ files\microchip\xc8\v2.41\avr\lib\gcc\avr\5.4.0\include\stddef.h:
